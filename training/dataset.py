d8230d8245fec0047c2246ced6448f6dd5b31459
Trong <trongnv.bi12-449@st.usth.edu.vn>
2024-05-23 09:23:06 +0700

    commit

--- training/dataset.py
+++ training/dataset.py
@@ -4,13 +4,16 @@ from transformers import T5Tokenizer
 from typing import Mapping, Tuple
 
 class QGDataset(torch.utils.data.Dataset):
-    def __init__(
-        self,
-        json_file: str,
-        max_length: int,
-        pad_mask_id: int,
-        tokenizer: T5Tokenizer
-    ) -> None:
+    def __init__(self, json_file: str, max_length: int, pad_mask_id: int, tokenizer: T5Tokenizer) -> None:
+        """
+        task:
+            - input: article (i.e. context)
+            - output: question <sep> answer
+        args:
+            tokenizer: tokenizer
+            data_split: train, validation, test
+        """
+        
         with open(json_file, 'r', encoding='utf-8') as f:
             self.data = json.load(f)
         self.max_length = max_length
@@ -24,26 +27,94 @@ class QGDataset(torch.utils.data.Dataset):
         item = self.data[index]
         context = item["context"]
         question_type = item["question_type"]
-        
+        separator = '<sep>'
         
         if question_type == "multiple_choice":
             question = item["question"]
-            options = " ".join([f"{chr(65+i)}: {opt}" for i, opt in enumerate(item["options"])])
-            answer_index = ord(item["answer"]) - 65
-            answer = item["options"][answer_index]
-            target_text = f"{question}\n{answer}\n{options}"
+            all_answers = item['options']
+            correct_answer_index =  ord(item['answer']) - 65
+
+            curr_correct = all_answers.pop(correct_answer_index)
+            target_text = question + ' ' + separator + ' ' + curr_correct
+            input_ids, attention_mask = self._encode_text(f"Trắc nghiệm: {context}")
             
         elif question_type == "sentences":
             question = item["question"]
             answer = item["answer"]
-            target_text = f"{question}\n{answer}"
-        
-        input_text = f"{context}\n"
+            target_text = question + ' ' + separator + ' ' + answer
+            input_ids, attention_mask = self._encode_text(f"Tự luận: {context}")
 
-        input_ids, attention_mask = self._encode_text(f"qa: {input_text}")
+        
         labels, _ = self._encode_text(target_text)
         masked_labels = self._mask_label_padding(labels)
 
+        return {
+            "input_ids": input_ids,
+            "attention_mask": attention_mask,
+            "labels": masked_labels
+        }
+
+    def _encode_text(self, text: str) -> Tuple[torch.Tensor, torch.Tensor]:
+        encoded_text = self.tokenizer(
+            text,
+            padding="max_length",
+            max_length=self.max_length,
+            truncation=True,
+            return_tensors='pt'
+        )
+        return (
+            encoded_text["input_ids"].squeeze(),
+            encoded_text["attention_mask"].squeeze()
+        )
+
+    def _mask_label_padding(self, labels: torch.Tensor) -> torch.Tensor:
+        labels[labels == self.tokenizer.pad_token_id] = self.pad_mask_id
+        return labels
+        
+        
+class DistractorDataset(torch.utils.data.Dataset):
+    def __init__(self, json_file: str, max_length: int, pad_mask_id: int, tokenizer: T5Tokenizer) -> None:
+        """
+        task:
+            - input: question <sep> answer <sep> context
+            - output: distractor1 <sep> distractor2 <sep> distractor3
+        args:
+            tokenizer: tokenizer
+            data_split: train, validation, test
+        """
+        
+        with open(json_file, 'r', encoding='utf-8') as f:
+            data = json.load(f)
+        self.data = [item for item in data if item["question_type"] == "multiple_choice"]
+        self.max_length = max_length
+        self.pad_mask_id = pad_mask_id
+        self.tokenizer = tokenizer
+
+    def __len__(self) -> int:
+        return len(self.data)
+
+    def __getitem__(self, index: int) -> Mapping[str, torch.Tensor]:
+        item = self.data[index]
+        context = item["context"]
+        question_type = item["question_type"]
+        separator = '<sep>'
+        
+        if question_type == "multiple_choice":
+            question = item["question"]
+            all_answers = item['options']
+            correct_answer_index =  ord(item['answer']) - 65
+
+            curr_correct = all_answers.pop(correct_answer_index)
+            curr_incorrect1 = all_answers[0]
+            curr_incorrect2 = all_answers[1]
+            curr_incorrect3 = all_answers[2]
+            target_text = curr_incorrect1 + ' ' + separator + ' ' + curr_incorrect2 + ' ' + separator + ' ' + curr_incorrect3
+            input_text = question + ' ' + separator + ' ' + curr_correct + ' ' + separator + ' ' + context
+
+        input_ids, attention_mask = self._encode_text(input_text)
+        labels, _ = self._encode_text(target_text)
+        masked_labels = self._mask_label_padding(labels)
+        
         return {
             "input_ids": input_ids,
             "attention_mask": attention_mask,
---
+++ training/train_distractors.py
@@ -0,0 +1,80 @@
+import argparse
+from transformers import T5Config, T5ForConditionalGeneration, T5Tokenizer
+
+from dataset import DistractorDataset
+from trainer import Trainer
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dataloader_workers", type=int, default=2)
+    parser.add_argument("--device", type=str, default="cuda")
+    parser.add_argument("--epochs", type=int, default=5)
+    parser.add_argument("--learning_rates", nargs="+", type=float, default=[2e-5])
+    parser.add_argument("--max_length", type=int, default=512)
+    parser.add_argument("--pad_mask_id", type=int, default=-100)
+    parser.add_argument("--qg_model", type=str, default="VietAI/vit5-base")
+    parser.add_argument("--pin_memory", dest="pin_memory", action="store_true", default=False)
+    parser.add_argument("--save_dir", type=str, default="./t5-base-question-generator")
+    parser.add_argument("--train_batch_size", type=int, default=4)
+    parser.add_argument("--valid_batch_size", type=int, default=4)
+    parser.add_argument("--log_file", type=str, default="result/train_distractor_log.csv")
+    parser.add_argument("--train_file", type=str, default="datasets/train/qg_train.json")
+    parser.add_argument("--valid_file", type=str, default="datasets/validation/qg_valid.json")
+    return parser.parse_args()
+
+
+def get_tokenizer(checkpoint: str) -> T5Tokenizer:
+    tokenizer = T5Tokenizer.from_pretrained(checkpoint)
+    return tokenizer
+
+
+def get_model(checkpoint: str, device: str, tokenizer: T5Tokenizer) -> T5ForConditionalGeneration:
+    # config = T5Config(decoder_start_token_id=tokenizer.pad_token_id)
+    # model = T5ForConditionalGeneration(config).from_pretrained(checkpoint)
+    
+    config = T5Config.from_pretrained(checkpoint)
+    model = T5ForConditionalGeneration.from_pretrained(checkpoint, config=config)
+
+    model.resize_token_embeddings(len(tokenizer))
+    model = model.to(device)
+    return model
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    tokenizer = get_tokenizer(args.qg_model)
+    
+    train_set = DistractorDataset(
+        json_file=args.train_file,
+        max_length=args.max_length,
+        pad_mask_id=args.pad_mask_id,
+        tokenizer=tokenizer
+    )
+
+    valid_set = DistractorDataset(
+        json_file=args.valid_file,
+        max_length=args.max_length,
+        pad_mask_id=args.pad_mask_id,
+        tokenizer=tokenizer
+    )
+    
+    for lr in args.learning_rates:
+        print(f"Training with learning rate: {lr}")
+        model = get_model(args.qg_model, args.device, tokenizer)
+        trainer = Trainer(
+            dataloader_workers=args.dataloader_workers,
+            device=args.device,
+            epochs=args.epochs,
+            learning_rate=lr,
+            model=model,
+            pin_memory=args.pin_memory,
+            save_dir=args.save_dir,
+            tokenizer=tokenizer,
+            train_batch_size=args.train_batch_size,
+            train_set=train_set,
+            valid_batch_size=args.valid_batch_size,
+            valid_set=valid_set,
+            log_file=args.log_file
+        )
+        trainer.train()
