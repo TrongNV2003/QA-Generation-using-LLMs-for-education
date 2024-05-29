5ad283b784465093198982ef0e46d277a8036d3d
Trong <trongnv.bi12-449@st.usth.edu.vn>
2024-05-29 15:45:51 +0700

    change prefix

--- training/data_loader.py
+++ training/data_loader.py
@@ -9,7 +9,7 @@ class QGDataset(torch.utils.data.Dataset):
         """
         task:
             - input: article (i.e. context)
-            - output: question <sep> answer (sentences type)
+            - output: question <sep> answer
         args:
             tokenizer: tokenizer
             data_split: train, validation, test
@@ -30,11 +30,12 @@ class QGDataset(torch.utils.data.Dataset):
     def __getitem__(self, index: int) -> Mapping[str, torch.Tensor]:
         item = self.data[index]
         context = item["context"]
+
         question = item["question"]
         answer = item["answer"]
         
-        target_text = question + ' ' + self.separator + ' ' + answer
-        input_ids, attention_mask = self._encode_text(f"qa: {context}")
+        target_text = f"Question: {question} {self.separator} Answer: {answer}"
+        input_ids, attention_mask = self._encode_text(f"Essay: {context}")
 
         labels, _ = self._encode_text(target_text)
         masked_labels = self._mask_label_padding(labels)
@@ -68,7 +69,7 @@ class MCQ_QGDataset(torch.utils.data.Dataset):
         """
         task:
             - input: article (i.e. context)
-            - output: question <sep> answer (MCQ type)
+            - output: question <sep> answer
         args:
             tokenizer: tokenizer
             data_split: train, validation, test
@@ -94,8 +95,8 @@ class MCQ_QGDataset(torch.utils.data.Dataset):
         label_answer =  item["answer"]
         answer = options[self.label_mapping[label_answer]]
             
-        target_text = question + ' ' + self.separator + ' ' + answer
-        input_ids, attention_mask = self._encode_text(f"qa: {context}")
+        target_text = f"Question: {question} {self.separator} Answer: {answer}"
+        input_ids, attention_mask = self._encode_text(f"Multiple choice: {context}")
 
         labels, _ = self._encode_text(target_text)
         masked_labels = self._mask_label_padding(labels)
@@ -165,8 +166,8 @@ class DistractorDataset(torch.utils.data.Dataset):
             random.shuffle(distractor_ids)
         distractors = [options[i] for i in distractor_ids]
         
-        input_text = question + ' ' + self.separator + ' ' + answer + ' ' + self.separator + ' ' + context
-        target_text = distractors[0] + ' ' + self.separator + ' ' + distractors[1] + ' ' + self.separator + ' ' + distractors[2]
+        input_text = f"Question: {question} {self.separator} Answer: {answer} {self.separator} Context: {context}"
+        target_text = f"Distractor: {distractors[0]} {self.separator} Distractor: {distractors[1]} {self.separator} Distractor: {distractors[2]}"
 
         input_ids, attention_mask = self._encode_text(input_text)
         labels, _ = self._encode_text(target_text)
