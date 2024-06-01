242765ea4a901b29420045c7f20a3e7366c38496
Trong <trongnv.bi12-449@st.usth.edu.vn>
2024-06-02 04:22:26 +0700

    finish update UI

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask, render_template, request
-from questiongenerator import QuestionGenerator
+from flask import Flask, render_template, request, jsonify
+from questiongenerator import QuestionAnswerGenerator
+import threading
+import time
+import uuid
 
-qg = QuestionGenerator()
-
+qg = QuestionAnswerGenerator()
 app = Flask(__name__)
+
+tasks = {}
 
 @app.route('/')
 def home():
@@ -11,18 +15,40 @@ def home():
 
 @app.route('/generate_questions', methods=['POST'])
 def generate_questions():
-    article = request.form['article']
-    num_questions = int(request.form['num_questions'])
-    answer_style = request.form['answer_style']
+    data = request.json
+    article = data['article']
+    num_questions = int(data['num_questions'])
+    answer_style = data['answer_style']
 
-    qa_list = qg.generate(article, num_questions=num_questions, answer_style=answer_style)
+    task_id = str(uuid.uuid4())
+    tasks[task_id] = {
+        'status': 'PENDING',
+        'result': None
+    }
+
+    thread = threading.Thread(target=generate_questions_task, args=(task_id, article, num_questions, answer_style))
+    thread.start()
+
+    return jsonify({"task_id": task_id}), 202
+
+@app.route('/task_status/<task_id>')
+def task_status(task_id):
+    task = tasks.get(task_id, None)
+    if not task:
+        return jsonify({'state': 'FAILURE', 'status': 'Task not found'})
+
+    return jsonify(task)
+
+def generate_questions_task(task_id, article, num_questions, answer_style):
+    # Chuyển mô hình sang GPU (nếu chưa)
+    qg.qg_model.to(qg.device)
+    qg.mcq_qg_model.to(qg.device)
+    qg.qd_model.to(qg.device)
     
-    if answer_style == 'sentences':
-        return render_template('questions.html', qa_list=qa_list)
-    elif answer_style == 'multiple_choice':
-        return render_template('questions_mcq.html', qa_list=qa_list)
-    else:
-        return "Invalid answer style"
+    tasks[task_id]['status'] = 'IN_PROGRESS'
+    qa_list = qg.generate(article, num_questions=num_questions, answer_style=answer_style)
+    tasks[task_id]['status'] = 'SUCCESS'
+    tasks[task_id]['result'] = qa_list
 
 if __name__ == '__main__':
     app.run(debug=True)
