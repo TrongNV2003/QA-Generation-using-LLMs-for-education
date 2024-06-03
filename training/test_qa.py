3d9ce4b1b9da01ab5fa69aa91edf7139f19f286c
Trong <trongnv.bi12-449@st.usth.edu.vn>
2024-06-03 13:51:57 +0700

    Add testing model

--- training/test_qa.py
+++ training/test_qa.py
@@ -1,10 +1,9 @@
 import argparse
 from transformers import T5Config, T5ForConditionalGeneration, T5Tokenizer
 
-from data_loader import QGDataset
+from data_loader import QGDataset, MCQ_QGDataset, DistractorDataset
 from trainer import Trainer
 import pandas as pd
-
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
@@ -14,21 +13,19 @@ def parse_args() -> argparse.Namespace:
     parser.add_argument("--learning_rate", type=float, default=0)
     parser.add_argument("--max_length", type=int, default=512)
     parser.add_argument("--pad_mask_id", type=int, default=-100)
-    parser.add_argument("--qg_model", type=str, default="./t5-base-question-generator")
+    parser.add_argument("--qa_model", type=str, default="./t5-base-question-generator")
+    parser.add_argument("--qa_mcq_model", type=str, default="./t5-base-question-mcq-generator")
+    parser.add_argument("--distractor_model", type=str, default="./t5-base-distractor-generator")
     parser.add_argument("--pin_memory", dest="pin_memory", action="store_true", default=False)
     parser.add_argument("--train_batch_size", type=int, default=4)
     parser.add_argument("--test_batch_size", type=int, default=4)
     parser.add_argument("--log_file", type=str, default="result/test_qg_log.csv")
     return parser.parse_args()
 
-
 def get_tokenizer(checkpoint: str) -> T5Tokenizer:
     tokenizer = T5Tokenizer.from_pretrained(checkpoint)
-    tokenizer.add_special_tokens(
-        {'additional_special_tokens': ['<sep>']}
-    )
+    tokenizer.add_special_tokens({'additional_special_tokens': ['<sep>']})
     return tokenizer
-
 
 def get_model(checkpoint: str, device: str, tokenizer: T5Tokenizer) -> T5ForConditionalGeneration:
     config = T5Config.from_pretrained(checkpoint)
@@ -37,42 +34,62 @@ def get_model(checkpoint: str, device: str, tokenizer: T5Tokenizer) -> T5ForCond
     model = model.to(device)
     return model
 
-
-if __name__ == "__main__":
+def main():
     args = parse_args()
-    tokenizer = get_tokenizer(args.qg_model)
 
-    train_set = QGDataset(
-        csv_file='datasets/train/qg_train.csv',
-        max_length=args.max_length,
-        pad_mask_id=args.pad_mask_id,
-        tokenizer=tokenizer
-    )
+    model_configs = [
+        {"type": "Model sentences", "checkpoint": args.qa_model, "dataset_class": QGDataset},
+        {"type": "Model multiple choice", "checkpoint": args.qa_mcq_model, "dataset_class": MCQ_QGDataset},
+        {"type": "Model distractor", "checkpoint": args.distractor_model, "dataset_class": DistractorDataset}
+    ]
 
-    test_set = QGDataset(
-        csv_file='datasets/test/qg_test.csv',
-        pad_mask_id=args.pad_mask_id,
-        max_length=args.max_length,
-        tokenizer=tokenizer
-    )
+    results = []
+
+    for config in model_configs:
+        tokenizer = get_tokenizer(config["checkpoint"])
+        train_set = config["dataset_class"](
+            json_file='datasets/train/qg_train.json',
+            max_length=args.max_length,
+            pad_mask_id=args.pad_mask_id,
+            tokenizer=tokenizer
+        )
+        test_set = config["dataset_class"](
+            json_file='datasets/test/qg_test.json',
+            pad_mask_id=args.pad_mask_id,
+            max_length=args.max_length,
+            tokenizer=tokenizer
+        )
+        model = get_model(config["checkpoint"], args.device, tokenizer)
+        trainer = Trainer(
+            dataloader_workers=args.dataloader_workers,
+            device=args.device,
+            epochs=args.epochs,
+            learning_rate=args.learning_rate,
+            model=model,
+            tokenizer=tokenizer,
+            pin_memory=args.pin_memory,
+            save_dir="",
+            train_batch_size=args.train_batch_size,
+            train_set=train_set,
+            valid_batch_size=args.test_batch_size,
+            valid_set=test_set,
+            log_file=args.log_file,
+            evaluate_on_accuracy=True
+        )
+
+        loss = trainer.evaluate(trainer.valid_loader)
+        accuracy = trainer.qg_accuracy(trainer.valid_loader)
+        results.append({"model_type": config["type"], "loss": round(loss, 3), "accuracy": round(accuracy*10, 3)})
+
+    for result in results:
+        print(f"Model type: {result['model_type']}")
+        print(f"Loss: {result['loss']}")
+        print(f"Accuracy: {result['accuracy']}")
+
+    # Lưu kết quả vào file CSV
+    df = pd.DataFrame(results)
+    df.to_csv(args.log_file, index=False)
+    print(f"Results saved to {args.log_file}")
     
-    model = get_model(args.qg_model, args.device, tokenizer)
-    trainer = Trainer(
-        dataloader_workers=args.dataloader_workers,
-        device=args.device,
-        epochs=args.epochs,
-        learning_rate=args.learning_rate,
-        model=model,
-        tokenizer=tokenizer,
-        pin_memory=args.pin_memory,
-        save_dir="",
-        train_batch_size=args.train_batch_size,
-        train_set=train_set,
-        valid_batch_size=args.test_batch_size,
-        valid_set=test_set,
-        log_file=args.log_file,
-        evaluate_on_accuracy=True
-    )
-    
-    trainer.evaluate(trainer.valid_loader)
-    trainer.qg_accuracy(trainer.valid_loader)
+if __name__ == "__main__":
+    main()
