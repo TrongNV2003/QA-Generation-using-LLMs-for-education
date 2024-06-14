5c596b6373c96f0aabe5cc0c9fda476ed0c90146
Trong <trongnv.bi12-449@st.usth.edu.vn>
2024-06-14 14:18:02 +0700

    change saving result file

--- training/train_distractors.py
+++ training/train_distractors.py
@@ -28,7 +28,7 @@ def parse_args() -> argparse.Namespace:
     parser.add_argument("--save_dir", type=str, default="./t5-base-distractor-generator")
     parser.add_argument("--train_batch_size", type=int, default=4)
     parser.add_argument("--valid_batch_size", type=int, default=4)
-    parser.add_argument("--log_file", type=str, default="result/train_distractor_log.csv")
+    parser.add_argument("--log_file", type=str, default="result/qg_training_distractor_log.csv")
     parser.add_argument("--train_file", type=str, default="datasets/train/qg_train.json")
     parser.add_argument("--valid_file", type=str, default="datasets/validation/qg_valid.json")
     parser.add_argument("--seed", type=int, default=42)
