3cb9588a5626cc5f16c31726f4420314660782c4
Trong <trongnv.bi12-449@st.usth.edu.vn>
2024-06-19 16:08:38 +0700

    update

--- plot_test_model_result.py
+++ plot_test_model_result.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 # run test_qa.py first, then this model will plot the result of test model
 
-def plot_results(csv_file: str, output_file: str) -> None:
+def plot_results(csv_file: str) -> None:
     # Đọc dữ liệu từ file CSV
     df = pd.read_csv(csv_file)
 
@@ -25,23 +25,18 @@ def plot_results(csv_file: str, output_file: str) -> None:
     # Thiết lập tên trục x
     plt.xticks(x, df['model_type'], ha='center')
 
-    # Thiết lập tiêu đề
-    plt.title('Loss and Accuracy of Different Models')
-
     # Thêm chú thích (legend)
     ax1.legend(loc='upper left')
 
     # Lưu biểu đồ thành file
     plt.tight_layout()
-    plt.savefig(output_file)
-    print(f"Plot saved to {output_file}")
 
 if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser(description='Plot results from CSV')
     parser.add_argument('--csv_file', type=str, default="result/test_qg_log.csv", help='Path to the CSV file containing the results')
-    parser.add_argument('--output_file', type=str, default="result/plot.png", help='Path to save the output plot image')
     args = parser.parse_args()
 
-    plot_results(args.csv_file, args.output_file)
+    plot_results(args.csv_file)
+    plt.show()
